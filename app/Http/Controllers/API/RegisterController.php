b4f0c82c279e45eacbc85686c1cd7ac96188ec4d
nirajbyanju <112626660+nirajbyanju@users.noreply.github.com>
2024-07-20 21:38:30 +0545

    hello

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,70 @@
+<?php
+   
+namespace App\Http\Controllers\API;
+   
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\Rules;
+   
+class RegisterController extends BaseController
+{
+    /**
+     * Register api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function register(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+           'firstName' => ['required', 'string', 'max:255'],
+            'lastName' => ['required', 'string', 'max:255'],
+            'userName' => ['required', 'string', 'max:255', 'unique:users,username'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'phoneNumber' => ['required', 'integer', 'unique:users,phone_number'],
+            'password' => ['required', Rules\Password::defaults(), 'same:cPassword'],
+            'cPassword' => ['required', Rules\Password::defaults()],
+        ]);
+   
+        if($validator->fails()){
+            return $this->sendError('Validation Error.', $validator->errors());       
+        }
+   
+        $input = $request->all();
+        $input['first_name'] = $request->input('firstName');
+        $input['last_name'] = $request->input('lastName');
+        $input['email'] = $request->input('email');
+        $input['username'] = $request->input('userName');
+        $input['phone_number'] = $request->input('phoneNumber');
+        $input['password'] = bcrypt($input['password']);
+        $input['status'] = '1';
+        
+        $user = User::create($input);
+        $success['token'] =  $user->createToken('MyApp')->plainTextToken;
+        $success['name'] =  $user->name;
+   
+        return $this->sendResponse($success, 'User register successfully.');
+    }
+   
+    /**
+     * Login api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request): JsonResponse
+    {
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){ 
+            $user = Auth::user(); 
+            $success['token'] =  $user->createToken('MyApp')->plainTextToken; 
+            $success['name'] =  $user->name;
+   
+            return $this->sendResponse($success, 'User login successfully.');
+        } 
+        else{ 
+            return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        } 
+    }
+}
