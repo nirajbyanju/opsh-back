f577f220167cdd609716b377d83b8152379f1630
nirajbyanju <112626660+nirajbyanju@users.noreply.github.com>
2024-07-22 01:01:21 +0545

    niraj

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -85,7 +85,7 @@ class RegisterController extends BaseController
     if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
         $user = Auth::user();
         $success['token'] = $user->createToken('MyApp')->plainTextToken;
-        $success['name'] = $user->name;
+        $success['name'] = $user;
 
         return $this->sendResponse($success, 'User login successfully.');
     } else {
