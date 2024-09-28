c0ab64c4afed7996e271ea47cfce9aa8e07be20e
nirajbyanju <112626660+nirajbyanju@users.noreply.github.com>
2024-09-28 12:44:32 +0545

    new

--- database/seeders/categorySeeder.php
+++ database/seeders/categorySeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\Category;
 
-class categorySeeder extends Seeder
+class CategorySeeder extends Seeder
 {
     /**
      * Run the database seeds.
